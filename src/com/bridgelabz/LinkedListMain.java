e503520b2466e3ce965f61640e3d62cc8a5fd341
Prohit <udkarprohit@gmail.com>
2023-01-31 09:56:55 +0530

    Added: Deleting element from ending of the linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,14 +4,14 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
         //Creating Object of LinkedList class.
-        System.out.println("Deletion From Start:- ");
-        LinkedList<Integer> linkedListFour = new LinkedList<>();
-        linkedListFour.push(70);
-        linkedListFour.push(30);
-        linkedListFour.push(56);
-        linkedListFour.deleteFromStart();
+        System.out.println("Deletion From End :- ");
+        LinkedList<Integer> linkedListFive = new LinkedList<>();
+        linkedListFive.push(70);
+        linkedListFive.push(30);
+        linkedListFive.push(56);
+        linkedListFive.deleteFromEnd();
 
-        linkedListFour.display();
+        linkedListFive.display();
 
     }
 }
