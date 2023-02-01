b9b1fae6e3f11673c6170c29ff136dfda3624e1b
Prohit <udkarprohit@gmail.com>
2023-02-01 10:33:06 +0530

    Added: Displaying size of the linked-list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,16 +4,14 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
         //Creating Object of LinkedList class.
-        System.out.println("Searched Element :- ");
-        LinkedList<Integer> linkedListSix = new LinkedList<>();
-        linkedListSix.push(70);
-        linkedListSix.push(30);
-        linkedListSix.push(56);
-        boolean isInserted = linkedListSix.insertAfter(40, 30);
-        linkedListSix.display();
-        if (isInserted)
-            System.out.println("Element inserted successfully");
-        else
-            System.out.println("Element not inserted");
+        LinkedList linkedList4 = new LinkedList();
+        linkedList4.push(70);
+        linkedList4.push(40);
+        linkedList4.push(30);
+        linkedList4.push(56);
+        linkedList4.display();
+        System.out.println(linkedList4.delete(40)+" is deleted");
+        linkedList4.display();
+        linkedList4.size();
     }
 }
