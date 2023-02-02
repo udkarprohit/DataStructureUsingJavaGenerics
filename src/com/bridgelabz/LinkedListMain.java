8e681284a8e25a50d7e02c8bb656e6accd92cdd5
Prohit <udkarprohit@gmail.com>
2023-02-02 08:59:12 +0530

    Added: Pop from the Stack till it is empty

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,11 +5,17 @@ import com.bridgelabz.stack.Stack;
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        //Creating Object of LinkedList class.
+        //Creating Object of Stack class.
         Stack<Integer> stack = new Stack<>();
         stack.push(70);
         stack.push(30);
         stack.push(56);
+        stack.display();
+        if (stack.pop()){
+            System.out.println("Stack is => " + stack.top);
+        } else {
+            System.out.println("Stack is not empty =>" + stack.top.data);
+        }
         stack.display();
     }
 }
